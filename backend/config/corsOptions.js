8bd8599914d6e8f85c7aefdc7716aaaeddabe9d4
Jitto Joyes <jitto@qburst.com>
2023-05-02 14:39:18 +0530

    Changes

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,7 +1,7 @@
 const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
-    origin: "https://talk-talk.onrender.com/",
+    origin: "https://talk-talk.onrender.com",
     credentials: true,
     optionsSuccessStatus: 200
 }
